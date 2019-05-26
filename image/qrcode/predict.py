5af5c54c25959f3157f6ac0de6e59de0fc4d99d7
wudizhangzhi <wudizhangzhi@163.com>
2019-05-26 21:21:54 +0800

    debug create docker image

--- image/qrcode/predict.py
+++ image/qrcode/predict.py
@@ -4,9 +4,9 @@ from models import cnn_ctc
 from captcha_utils.generator import TextImageGenerator
 
 
-def junka_predict_one(captcha, weights=None):
-    model, test_func = cnn_ctc.junka_model()
-    model.load_weights(weights)
+def junka_predict_one(captcha, test_func):
+    # model, test_func = cnn_ctc.junka_model()
+    # model.load_weights(weights)
     captcha = TextImageGenerator.formatCaptcha(captcha)
     captcha = np.expand_dims(captcha, axis=0)
     config = cnn_ctc.config
@@ -15,11 +15,10 @@ def junka_predict_one(captcha, weights=None):
     ret = cnn_ctc.decode_batch(test_func, TextImageGenerator.labels_to_text, x)
     return ret
 
-
-if __name__ == '__main__':
-    import cv2
-
-    weight_path = 'E:\\Workplace\\bdzh\\MachineLearning\\SmallCaptcha\\image_ocr\\2019_05_24_21_49_38\\weights70_acc_87.00000.h5'
-    test_path = 'E:\\test.png'
-    captcha = cv2.imread(test_path)
-    print(junka_predict_one(captcha, weight_path)[0])
+# if __name__ == '__main__':
+#     import cv2
+#
+#     weight_path = 'E:\\Workplace\\bdzh\\MachineLearning\\SmallCaptcha\\image_ocr\\2019_05_24_21_49_38\\weights70_acc_87.00000.h5'
+#     test_path = 'E:\\test.png'
+#     captcha = cv2.imread(test_path)
+#     print(junka_predict_one(captcha, weight_path)[0])
---
+++ image/qrcode/web_server.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request
+import numpy as np
+import cv2
+from models import cnn_ctc
+from predict import junka_predict_one
+from captcha_utils.generator import TextImageGenerator
+import json
+import local_settings as settings
+
+app = Flask(__name__)
+
+# init_model
+weights = settings.weight_path
+model, test_func = cnn_ctc.junka_model()
+model.load_weights(weights)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    return 'hello'
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    """预测的结果
+
+    """
+    file = request.files['files']
+    filestr = file.read()
+    # convert string data to numpy array
+    npimg = np.fromstring(filestr, np.uint8)
+    # convert numpy array to image
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    # TODO 预测验证码
+    ret = junka_predict_one(img, test_func)[0]
+
+    return json.dumps({"data": ret})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=2000)
